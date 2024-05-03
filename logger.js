bda230100d074c80a3e600871cb02b652c4344ba
Mykola Mazurenko <nikolaym@comm-it.com>
2024-05-03 15:07:07 +0300

    make logging to file as optional

--- logger.js
+++ logger.js
@@ -4,18 +4,23 @@ var winston = require('winston');
  * `winston.transports.MongoDB`
  */
 var settings = require('./settings');
+var transports = [
+    new winston.transports.Console({ json: false, timestamp: true, level: 'verbose' })
+];
 
-var logger = new (winston.Logger)({
-    transports: [
-        new (winston.transports.Console)({ json: false, timestamp: true, level: 'verbose' }),
-        new winston.transports.File({
-            json: false,
-            level: 'verbose',
-            filename: settings.logFile,
-            handleExceptions: true,
-            maxsize: 4000000
-        })
-    ],
+var isLoggingToFileDisabled = process.env.DISABLE_LOGGING_TO_FILE === 'true';
+if (!isLoggingToFileDisabled) {
+    transports.push(new winston.transports.File({
+        json: false,
+        level: 'verbose',
+        filename: settings.logFile,
+        handleExceptions: true,
+        maxsize: 4000000
+    }));
+}
+
+var logger = new winston.Logger({
+    transports: transports,
     exitOnError: false
 });
 
