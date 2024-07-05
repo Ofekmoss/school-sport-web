1490aa9a9ec07edde4fad8ddb9812481061eba2b
Mykola Mazurenko <nikolaym@comm-it.com>
2024-07-05 12:47:54 +0300

    update CORS configuration

--- v2/api/index.js
+++ v2/api/index.js
@@ -9,9 +9,10 @@ var router = express.Router();
 
 // TODO - remove when not needed anymore
 router.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     next();
 });
 
--- v2/api/manage/seasons.js
+++ v2/api/manage/seasons.js
@@ -9,7 +9,7 @@ var router = express.Router();
 
 // TODO - remove when not needed anymore
 router.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
