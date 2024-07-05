4ffe6f0d24551c399d2c722fcfd146b5ce8caffc
Mykola Mazurenko <nikolaym@comm-it.com>
2024-07-05 11:19:50 +0300

    updated stop/start script

--- ci-cd/application-start.sh
+++ ci-cd/application-start.sh
@@ -3,4 +3,4 @@ echo "Restart services with new docker images"
 
 cd /var/www/html/school-sport-web
 echo "My location is $(pwd)"
-docker compose pull && docker compose up -d
+docker compose pull && docker compose build && docker compose down && docker compose up -d
