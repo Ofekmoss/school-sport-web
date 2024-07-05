967400ab7277c9266f7c46baa5dc8f35769ef8d8
Mykola Mazurenko <nikolaym@comm-it.com>
2024-07-05 14:54:15 +0300

    test utodeploy to dev

--- ci-cd/application-start.sh
+++ ci-cd/application-start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "Restart services with new docker images"
-
 cd /var/www/html/school-sport-web
 echo "My location is $(pwd)"
-docker compose pull && docker compose build && docker compose down && docker compose up -d
+docker compose pull && docker compose build && docker compose down && docker compose up -d --build
