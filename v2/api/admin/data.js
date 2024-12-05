68a3d77d7e06f59ef3081f4cbd98a04ada081598
LavyAnisco <lavy@aniscoit.com>
2024-12-05 17:52:37 +0300

    feat: fetch raw categories, names, champ details

--- v2/api/admin/data.js
+++ v2/api/admin/data.js
@@ -20,4 +20,44 @@ router.get("/championships", util.requireRole("admin"), function (req, res) {
   }
 });
 
+router.get("/championships-details", util.requireRole("admin"), function (req, res) {
+  if (req.session.user) {
+    var options = {};
+    if (req.body.season) {
+      options.season = parseInt(req.body.season);
+    }
+
+    Championships.getRawChampionshipsDetails(options, function (err, result) {
+      util.sendResult(res, err, result);
+    });
+  } else {
+    util.sendResult(res, { status: 403 });
+  }
+});
+
+router.get("/category-names", util.requireRole("admin"), function (req, res) {
+  if (req.session.user) {
+    Championships.getRawCategoryNames(function (err, result) {
+      util.sendResult(res, err, result);
+    });
+  } else {
+    util.sendResult(res, { status: 403 });
+  }
+});
+
+router.get("/categories", util.requireRole("admin"), function (req, res) {
+  if (req.session.user) {
+    var options = {};
+    if (req.body.season) {
+      options.season = parseInt(req.body.season);
+    }
+
+    Championships.getRawCategories(options, function (err, result) {
+      util.sendResult(res, err, result);
+    });
+  } else {
+    util.sendResult(res, { status: 403 });
+  }
+});
+
 module.exports = router;
